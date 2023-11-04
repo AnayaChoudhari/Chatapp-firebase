b63894e8cae14a563923ee0f5838267b1607d49d
Anaya Choudhari <anayachoudhari@Anayas-MacBook-Air.local>
2023-11-04 15:11:44 +0530

    initial commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:100417368299:android:90758a7a8d577210f98eae";
+  static String apiKey = "AIzaSyCBhJTAbBBj4obFpuORLZHnCAbMyrcjNHs";
+  static String messagingSenderId = "100417368299";
+  static String projectId = "chatappflutter-8c89c";
+  final primaryColor = const Color(0xFFee7b64);
+}
